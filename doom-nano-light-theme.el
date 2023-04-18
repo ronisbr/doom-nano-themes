ced731dc6c047b566f36d802ce16d7117f71cec8
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2023-04-18 14:17:37 -0300

    :bookmark: Version 0.2.2

--- doom-nano-light-theme.el
+++ doom-nano-light-theme.el
@@ -10,7 +10,7 @@
 ;; Keywords         : mode-line, header-line
 ;; Package-Requires : ((emacs "28") (doom-themes "2"))
 ;; URL              : https://github.com/ronisbr/doom-nano-themes/
-;; Version          : 0.2.1
+;; Version          : 0.2.2
 ;;
 ;;; Usage:
 ;; 1. Copy this file to `~/.doom.d/themes/' `~/.config/doom/themes/`.
